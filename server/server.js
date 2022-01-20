6f0c2a8f9aa2d1408a6e80d7c673d5b5e19784a6
Chaitra <chaitra.srinivas@gmail.com>
2022-01-20 12:20:43 +1100

    Updated packages

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const express = require("express");
+const http = require("http");
+
+const PORT = 4000;
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+// A map of functions which return data for the schema.
+
+const resolvers = {
+  Query: {
+    hello: function () {
+      return "world";
+    },
+  },
+};
+
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+startApolloServer(typeDefs, resolvers);
