2577b708849e2515d66e49a25598443cbd3b032b
Chaitra <chaitra.srinivas@gmail.com>
2022-01-21 12:28:02 +1100

    Integrating React

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import ArticleList from "./components/articles/ArticleList";
+import ArticleInfo from "./components/articles/ArticleInfo";
+import ArticleAdd from "./components/articles/ArticleAdd";
+import ArticleEdit from "./components/articles/ArticleEdit";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         These are the users in the db
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navigation />
+        <div className='container'>
+          <Main />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+function Navigation() {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark mb-4'>
+      <div className='container'>
+        <ul className='navbar-nav mr-auto'>
+          <li className='nav-item'>
+            <NavLink exact className='nav-link' activeClassName='active' to='/'>
+              Home
+            </NavLink>
+          </li>
+          <li className='nav-item'>
+            <NavLink
+              exact
+              className='nav-link'
+              activeClassName='active'
+              to='/articles'>
+              Articles
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+function Main() {
+  return (
+    <Routes>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/articles' component={ArticleList} />
+      <Route exact path='/articles/new' component={ArticleAdd} />
+      <Route exact path='/articles/:_id' component={ArticleInfo} />
+      <Route exact path='/articles/:_id/edit' component={ArticleEdit} />
+    </Routes>
   );
 }
 
