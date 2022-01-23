d7f3323bad630eed52e305b376fb45f6d07e6dd1
Chaitra <chaitra.srinivas@gmail.com>
2022-01-23 17:04:46 +1100

    Getting prompts from a template

--- client/src/components/stories/StoryAdd.js
+++ client/src/components/stories/StoryAdd.js
@@ -1,54 +1,98 @@
-import { React } from "react";
+import { React , useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import {  useMutation } from "@apollo/client";
+import { useMutation } from "@apollo/client";
 import { CREATE_STORY } from "../../utils/mutations";
 import { GET_STORIES } from "../../utils/queries";
+const templates = require("./templates.json");
 
 function StoryAdd(props) {
- 
   let { title, content } = useParams;
+  let selectedTemplate = templates[0];
+
+  const [selectedPage, setSelectedPage] = useState(selectedTemplate.pages[0])
+
+  console.log(selectedTemplate);
+  let pageVariables = selectedTemplate.pages.flatMap(
+    (page)=>
+      page.variables.map((v) => ({ id: page.id + '-' +  v.id, value: "1" }))
+     )
+ const [variableValues, setVariableValues] = useState(pageVariables)
+console.log(variableValues);
 
   const [createStory, { data, loading, error }] = useMutation(CREATE_STORY);
 
   if (loading) return "Loading...";
   if (error) return `Submission error! ${error.message}`;
 
+  
+
+  let pages = selectedTemplate.pages.map((page) => {
+    return <div key={page.id}>
+      <a href="#" onClick={(e)=>{e.preventDefault(); setSelectedPage(page);}}>{page.title}</a> <br/>
+      <span>{page.description}</span> <br/>
+    </div>;
+  });
+
+  let variables = selectedPage.variables.map((variable) => {
+    return <div key={variable.id}>
+      <input
+      value={variableValues.find((f) => f.id === selectedPage.id + '-' + variable.id).value}
+      onChange={e => setVariableValues(
+            [...variableValues].map( 
+              (m) => {
+          if (m.id == selectedPage.id + '-' + variable.id) {
+            return ({ id: selectedPage.id + '-' + variable.id, value : e.target.value});
+          } else 
+          {
+            return m;
+          }
+
+      }) ) }
+      placeholder={variable.description}></input>
+    </div>;
+  });
   return (
     <div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          createStory({ variables: { title: title.value, content: content.value } ,
-            refetchQueries: [{ query: GET_STORIES }]});
+          createStory({
+            variables: { title: title.value, content: content.value },
+            refetchQueries: [{ query: GET_STORIES }],
+          });
         }}>
-          <div className='form-group'>
-                <label>Title:</label>
-                <input
-                  type='text'
-                  className='form-control'
-                  ref={function (node) {
-                    return (title = node);
-                  }}
-                />
-              </div>
-              <div className='form-group'>
-                <label>Content:</label>
-                <textarea
-                  rows='5'
-                  className='form-control'
-                  ref={function (node) {
-                    return (content = node);
-                  }}
-                />
-              </div>
-              <p className='btn-group'>
-                <button type='submit' className='btn btn-primary'>
-                  Submit
-                </button>
-                <Link to='/stories' className='btn btn-secondary'>
-                  Cancel
-                </Link>
-              </p>
+        <div className='form-group'>
+          <label>Title:</label>
+          <input
+            type='text'
+            className='form-control'
+            ref={function (node) {
+              return (title = node);
+            }}
+          />
+        </div>
+        <div className='form-group'>
+          <label>Content:</label>
+          <textarea
+            rows='5'
+            className='form-control'
+            ref={function (node) {
+              return (content = node);
+            }}
+          />
+          {selectedPage.title}
+          {pages}
+          {variables}
+        </div>
+
+        <p className='btn-group'>
+          <button type='submit' className='btn btn-primary'>
+            Submit
+          </button>
+          <Link to='/stories' className='btn btn-secondary'>
+            Cancel
+          </Link>
+        </p>
       </form>
     </div>
   );
