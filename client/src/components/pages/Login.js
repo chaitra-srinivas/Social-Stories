ee7caf5edcc1166febe4d1160aa72e1061164671
Chaitra <chaitra.srinivas@gmail.com>
2022-02-04 13:48:52 +1100

    logo

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../../utils/mutations';
-import Navigation from './Navigation';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../../utils/mutations";
+import Navigation from "./Navigation";
 import "../../App.css";
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+} from "semantic-ui-react";
+import logo from "../images/SocialStories.gif";
 
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
@@ -38,71 +47,69 @@ const Login = (props) => {
 
     // clear form values
     setFormState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     });
   };
 
   return (
     <div>
-       <Navigation />
-       <Grid textAlign='center' style={{ height: '70vh' }} verticalAlign='middle'>
-       <Grid.Column style={{ maxWidth: 450 }}>
-      <Header id="loginHeader" as='h2' textAlign='center'>
-        <Image src='/logo.png' /> Log-in
-      </Header>
+      <Navigation />
+      <Grid
+        textAlign='center'
+        style={{ height: "70vh" }}
+        verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header id='loginHeader' as='h2' textAlign='center'>
+            <Image src={logo} /> Log-in
+          </Header>
           {data ? (
             <p>
-              Success! You may now head{' '}
-              <Link to="/stories">View avaliable stories</Link>
+              Success! You may now head{" "}
+              <Link to='/stories'>View avaliable stories</Link>
             </p>
           ) : (
             <Form size='large' onSubmit={handleFormSubmit}>
-               <Segment>
-            <Form.Input 
-            fluid icon='user'
-             iconPosition='left'
-             placeholder='E-mail address' 
-             name="email"
-             type='email'
-             value={formState.email}
-             onChange={handleChange}
-              />
-              <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            name="password"
-            type='password'
-            value={formState.password}
-            onChange={handleChange}
-          />
-           <Button id="btnSubmit"
-           color='teal' 
-           fluid size='large'
-           type='submit'
-           >
-            Login
-          </Button>
-          <Message id="msg">
-        New user? <Link to='/signup'>Sign Up</Link>
-      </Message>
-           </Segment>
-           </Form>
+              <Segment>
+                <Form.Input
+                  fluid
+                  icon='user'
+                  iconPosition='left'
+                  placeholder='E-mail address'
+                  name='email'
+                  type='email'
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <Form.Input
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  name='password'
+                  type='password'
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <Button
+                  id='btnSubmit'
+                  color='teal'
+                  fluid
+                  size='large'
+                  type='submit'>
+                  Login
+                </Button>
+                <Message id='msg'>
+                  New user? <Link to='/signup'>Sign Up</Link>
+                </Message>
+              </Segment>
+            </Form>
           )}
 
-          {error && (
-            <div id='errorMsg' >
-              {error.message}
-            </div>
-          )}
-         
-      
-    </Grid.Column>
-  </Grid>
+          {error && <div id='errorMsg'>{error.message}</div>}
+        </Grid.Column>
+      </Grid>
     </div>
-   
   );
 };
 
