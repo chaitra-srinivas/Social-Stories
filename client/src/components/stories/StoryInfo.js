6af6bff0cc14f66df2f547ca6f94d8de1c4fb494
Chaitra <chaitra.srinivas@gmail.com>
2022-02-04 23:10:45 +1100

    Story Info page

--- client/src/components/stories/StoryInfo.js
+++ client/src/components/stories/StoryInfo.js
@@ -3,15 +3,16 @@ import { useParams, Link, useNavigate } from "react-router-dom";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_STORY, GET_STORIES } from "../../utils/queries";
 import { DELETE_STORY } from "../../utils/mutations";
+import { Grid, Image } from "semantic-ui-react";
 
 function StoryInfo() {
   let { id } = useParams();
-  const { loading, data } = useQuery(GET_STORY, { variables: { id: id }});
+  const { loading, data } = useQuery(GET_STORY, { variables: { id: id } });
   const singleStory = data?.story || {};
-  
+
   const [deleteStory, { dataLoading, error }] = useMutation(DELETE_STORY, {
     variables: { id: singleStory.id },
-     refetchQueries: [GET_STORIES], 
+    refetchQueries: [GET_STORIES],
     /*  update(cache, { data: { deleteStory } }) {
       try {
         const stories = cache.readQuery({ query: GET_STORIES });
@@ -30,22 +31,33 @@ function StoryInfo() {
   if (error) return `Submission error! ${error.message}`;
 
   return (
-    <div>
+    <div className='ui container'>
       <h2>{singleStory.title}</h2>
       {singleStory.pages.map((page) => {
         return (
-          <div key={page.id} className='card mb-3 card-body bg-light p-2'>
-            <p>
-              <h6>{page.content}</h6>
-            </p>
-            <img
-              src={page.image}
-              alt='pageimage'
-              style={{ width: "200px", height: "150px" }}
-            />
-          </div>
+          <Grid columns={2} padded>
+            <Grid.Row>
+              <div id='pageContent' className='ui raised segment'>
+                <div key={page.id} className='item'>
+                  <div className='ui medium image'>
+                    <Image
+                      className='img'
+                      size='large'
+                      centered
+                      src={page.image}
+                      alt='pageimage'
+                    />
+                  </div>
+                  <div id='pContent' className='ui center aligned container'>
+                    <p>{page.content}</p>
+                  </div>
+                </div>
+              </div>
+            </Grid.Row>
+          </Grid>
         );
       })}
+
       <p className='btn-group'>
         <Link to={`/stories/${singleStory.id}/edit`} className='btn btn-info'>
           Edit
@@ -53,9 +65,8 @@ function StoryInfo() {
         <button
           className='btn btn-danger'
           onClick={() => {
-            deleteStory({
-            });
-           navigate("/stories/");
+            deleteStory({});
+            navigate("/stories/");
           }}>
           Delete
         </button>
