68c80a4534e45811dbf21a8aee6c9f049c9c3b83
Chaitra <chaitra.srinivas@gmail.com>
2022-03-19 15:40:13 +1100

    Semantic UI fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'semantic-ui-css/semantic.min.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
